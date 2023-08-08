66b58fea6c1c7ddc06ecb4016fd83d8f057165ea
Ovikx <ovikdas10@gmail.com>
2023-08-07 23:24:40 -0400

    add outward field

--- src/fields.rs
+++ src/fields.rs
@@ -4,3 +4,8 @@ pub fn inward_field(point: (i32, i32), _window_size: (u32, u32)) -> (f32, f32) {
     let magnitude = magnitude((point.0 as f32, point.1 as f32));
     (-point.0 as f32 / magnitude, -point.1 as f32 / magnitude)
 }
+
+pub fn outward_field(point: (i32, i32), _window_size: (u32, u32)) -> (f32, f32) {
+    let magnitude = magnitude((point.0 as f32, point.1 as f32));
+    (point.0 as f32 / magnitude, point.1 as f32 / magnitude)
+}
